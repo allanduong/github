e34edc41123f34132e292ecbfa1221965f87f1a8
exknown <abduong206@yahoo.com>
2019-10-01 15:03:26 -0700

    Update script.js
    Probably dont need this

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function navbar(){
-    window.onscroll = function () { myFunction() };
+// var navB = function navbar(){
+//     window.onscroll = function () { myFunction() };
 
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
+//     var navbar = document.getElementById("navbar");
+//     var sticky = navbar.offsetTop;
 
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-}
+//     function myFunction() {
+//       if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//       } else {
+//         navbar.classList.remove("sticky");
+//       }
+//     }
+// }
